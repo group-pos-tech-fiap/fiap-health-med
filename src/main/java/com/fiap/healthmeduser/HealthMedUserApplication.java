805b8cc5e0c084cb09ea56c55346debe81274007
Diego Silveira <diego_silveira@sicredi.com.br>
2024-09-27 17:59:41 -0300

    first commit p2

---
+++ src/main/java/com/fiap/healthmeduser/HealthMedUserApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.healthmeduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMedUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthMedUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiap/healthmeduser/HealthMedUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.healthmeduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthMedUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
